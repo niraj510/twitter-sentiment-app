884682da36472759760508c6c130ac8e34739415
niraj510 <60465283+niraj510@users.noreply.github.com>
2020-05-31 00:26:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -29,21 +29,39 @@ def tweets():
     api.set_tweetcount(tweetcount)
 
     tweets = api.get_tweets()
-    tweetsForcatter = api.get_tweets1()
+    tweetsForscatter = api.get_tweetsAllTweets()
     subject =[];
     polar = [];
-    subject=   api.getSubject(tweetsForcatter)
-    polar=   api.getPolar(tweetsForcatter)
+    subject=   api.getSubject(tweetsForscatter)
+    polar=   api.getPolar(tweetsForscatter)
     subject = subject.to_json()
     polar = polar.to_json()
     
     jsnobjectSubject =json.loads(subject)
     jsnobjectpolar =json.loads(polar)
-  
+    
+    #emotinal analysis
+    tweet_Data  = api.filter_twitter_data(tweetsForscatter)
+    emotion_plot= []
+    emotion_plot= api.generate_chart(tweet_Data)
+ 
+    #location data
+    location_data = api.getLocation();
+    location_data = location_data.to_json()
+   
+    #print("hi", location_data);
+    location_data = [['fo', 0],['us', 1],['jp', 2],['in', 3],['fr', 4],['cn', 5],['pt', 6],['sw', 7],['sh', 8],['ec', 9],['au', 10],
+['ph', 11],['es',12],['bu',13],['mv', 14],['sp', 15],['gb', 16],['gr', 17],['dk', 18],['gl', 19],['pr', 20],['um', 21],
+['vi', 22],['ca', 23],['ar', 24],['cl', 25],['cv', 26],['dm', 27],['sc', 28],['jm', 29],['om', 30],['vc', 31],['sb', 32],['lc', 33],['no', 34],
+['kn', 35],['bh', 36],['id', 37],['mu', 38],['se', 39],['ru', 40],['tt', 41],['br', 42],['bs', 43],['pw', 44],['gd', 45],['ag', 46],['fj', 47],
+['bb', 48],['it', 49],['mt', 50]]
+    
     print("{} tweets".format(len(tweets)))
     return jsonify({'data': tweets, 'count': len(tweets), 
                     'subject' : jsnobjectSubject,
-                    'polar' : jsnobjectpolar})
+                    'polar' : jsnobjectpolar,
+                    'emotion_plot' : emotion_plot,
+                    'location_data' : location_data})
 
 
 #port = int(os.environ.get('PORT', 5000))
--- twitter.py
+++ twitter.py
@@ -7,8 +7,13 @@ from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from langdetect import detect
 import pandas as pd
+import collections
+from collections import Counter
+import string
+from pathlib import Path
 
 recd_tweets = []
+
 
 class TwitterClient(object):
     '''
@@ -64,30 +69,6 @@ class TwitterClient(object):
             return 'negative'
         
         
-        
-        # polarity += analysis.sentiment.polarity  # adding up polarities to find the average later
-
-        # if (analysis.sentiment.polarity == 0):  # adding reaction of how people are reacting to find average later
-        #     neutral += 1
-        #     return neutral
-        # elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
-        #     wpositive += 1
-        #     return wpositive
-        # elif (analysis.sentiment.polarity > 0.3 and analysis.sentiment.polarity <= 0.6):
-        #     positive += 1
-        #     return positive
-        # elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
-        #     spositive += 1
-        #     return spositive
-        # elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
-        #     wnegative += 1
-        #     return wnegative
-        # elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity <= -0.3):
-        #     negative += 1
-        #     return negative
-        # elif (analysis.sentiment.polarity > -1 and analysis.sentiment.polarity <= -0.6):
-        #     snegative += 1
-        #     return snegative
     
     def get_tweet_sentiment_vader(self, tweet):
         #analysis = TextBlob(self.clean_tweet(tweet))
@@ -100,32 +81,6 @@ class TwitterClient(object):
         return 'negative'
         
      
-        
-        # polarity += analysis.sentiment.polarity  # adding up polarities to find the average later
-
-        # if (analysis.sentiment.polarity == 0):  # adding reaction of how people are reacting to find average later
-        #     neutral += 1
-        #     return neutral
-        # elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
-        #     wpositive += 1
-        #     return wpositive
-        # elif (analysis.sentiment.polarity > 0.3 and analysis.sentiment.polarity <= 0.6):
-        #     positive += 1
-        #     return positive
-        # elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
-        #     spositive += 1
-        #     return spositive
-        # elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
-        #     wnegative += 1
-        #     return wnegative
-        # elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity <= -0.3):
-        #     negative += 1
-        #     return negative
-        # elif (analysis.sentiment.polarity > -1 and analysis.sentiment.polarity <= -0.6):
-        #     snegative += 1
-        #     return snegative
-    
-
 
     def cleanTxt(self,text):
         text =re.sub(r'@[A-Za-z0-9]+','',text) #removing @ Mentios
@@ -133,8 +88,6 @@ class TwitterClient(object):
         text =re.sub(r'RT[\s]+','',text)     #removing RT
         text =re.sub(r'https?:\/\/\S+','',text) #removing the hyper link
         return text
-    
-   
     
    
     
@@ -171,7 +124,6 @@ class TwitterClient(object):
 
         
         tweets = []
-        recd_tweets1 = []
         unique_tweets = set()
 
         try:
@@ -200,18 +152,6 @@ class TwitterClient(object):
             if not recd_tweets:
                 pass
             
-            # polarity = 0
-            # positive = 0
-            # wpositive = 0
-            # spositive = 0
-            # negative = 0
-            # wnegative = 0
-            # snegative = 0
-            # neutral = 0   
-            
-            # recd_tweets1['recdtweets'] = recd_tweets
-            # print("recd_tweets1"+recd_tweets1['recdtweets'])
-            # tweets.append(recd_tweets1)
             for tweet in recd_tweets:
                 try:
                     if detect(tweet.text) != 'en':
@@ -223,6 +163,7 @@ class TwitterClient(object):
 
                 parsed_tweet['text'] = tweet.text
                 parsed_tweet['user'] = tweet.user.screen_name
+               
                 
                 if self.with_sentiment == 1:
                     parsed_tweet['sentiment'] = self.get_tweet_sentiment(
@@ -245,7 +186,7 @@ class TwitterClient(object):
         except tweepy.TweepError as e:
             print("Error : " + str(e))
             
-    def get_tweets1(self):
+    def get_tweetsAllTweets(self):
 
 
         try:
@@ -273,8 +214,158 @@ class TwitterClient(object):
 
             if not recd_tweets:
                 pass
-            
             return recd_tweets
 
         except tweepy.TweepError as e:
             print("Error : " + str(e))
+            
+    def getLocation(self):
+      
+        alltweets = self.get_tweetsAllTweets()
+        outtweets = [[tweet.created_at,tweet.entities["hashtags"],tweet.entities["user_mentions"],tweet.favorite_count,
+                      tweet.geo,tweet.id_str,tweet.lang,tweet.retweet_count,tweet.retweeted,tweet.source,tweet.text,
+                      tweet._json["user"]["location"],tweet._json["user"]["name"],tweet._json["user"]["time_zone"],
+                      tweet._json["user"]["utc_offset"]] for tweet in alltweets]
+        
+        
+        location_data = []
+        place = pd.Series([str(i[0]) for i in outtweets])
+        print("place",place);
+        location_data = pd.Series([str(i[12]) for i in outtweets])
+        
+        return(location_data)
+      
+    def filter_twitter_data(self,tweets):
+
+        id_list = [tweet.id for tweet in tweets]
+        tweet_Data = pd.DataFrame(id_list,columns=['id'])
+        tweet_Data["retweet_count"]= [tweet.retweet_count for tweet in tweets]
+       
+    
+    
+        Sentiments_list = []
+        Sentiments_group = []
+        Subjectivity_list = []
+        Subjectivity_group = []
+        tweet_text_list = []
+        tweet_source = []
+        tweet_translation= []
+        tweet_location_list = []
+        
+        for tweet in tweets:
+            raw_tweet_text = tweet.text
+            message = TextBlob(tweet.text)
+            location = tweet.author.location
+            source = tweet.source
+            tweet_source.append(source)
+            message = str(message)
+            new_message = ""
+    
+            for letter in range(0,len(message)):
+                current_read =message[letter]
+                if ord(current_read) > 126:
+                    continue
+                else:
+                    new_message =new_message+current_read
+    
+            message = new_message
+            tweet_translation.append(message)
+            #message = fix_pattern(message)
+            message = TextBlob(message)
+            
+            sentiment = message.sentiment.polarity
+            if (sentiment > 0):
+                Sentiments_group.append('positive')
+            elif (sentiment < 0):
+                Sentiments_group.append('negative')
+            else:
+                Sentiments_group.append('neutral')
+                
+            subjectivity = message.sentiment.subjectivity
+            if (subjectivity > 0.4):
+                Subjectivity_group.append('subjective')
+            else:
+                Subjectivity_group.append('objective')
+                
+            Sentiments_list.append(sentiment)
+            Subjectivity_list.append(subjectivity)
+            tweet_text_list.append(raw_tweet_text)
+            tweet_location_list.append(location)
+            
+            
+        tweet_Data["sentiments"] = Sentiments_list
+        tweet_Data["sentiments_group"] = Sentiments_group
+        tweet_Data["subjectivity"]= Subjectivity_list
+        tweet_Data["subjectivity_group"] = Subjectivity_group
+        tweet_Data["location"] = tweet_location_list
+        tweet_Data["text"] = tweet_text_list
+        tweet_Data["translate"] = tweet_translation
+    
+     
+        return (tweet_Data)
+    
+    def generate_chart(self,tweetsDataframe):
+
+
+       retweet_table = []
+        
+     
+    
+       retweet_table.append(["Tweets"])
+       df = tweetsDataframe[['translate']].drop_duplicates()[:6]
+       for key in df['translate']:
+          temp = [key]
+          retweet_table.append(temp)
+      
+    
+       new_list = []
+       for item in tweetsDataframe['translate']:
+       	new_item = [item]
+       	new_list.append(new_item)
+    
+       text = ""    
+       text_tweets = new_list
+       length = len(text_tweets)
+    
+       for i in range(0, length):
+        text = text_tweets[i][0] + " " + text
+    
+       lower_case = text.lower()
+       cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+       tokenized_words = cleaned_text.split()
+       #print(tokenized_words)
+    
+       stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+                   "will", "just", "don", "should", "now"]
+    
+       final_words = [word for word in tokenized_words if word not in stop_words]
+       emotion_list = []
+       with open('emotions.txt', 'r') as file:
+        	for line in file:
+        		clear_line = line.replace('\n', '').replace(',', '').replace("'", '').strip()
+        		word, emotion = clear_line.split(':')
+        		if word in final_words:
+        			emotion_list.append(emotion)
+    
+       w = Counter(emotion_list)
+       c = dict(w)
+    
+       emotion_keys = []
+       emotion_values = []
+    
+       for k,y in c.items():
+        	emotion_keys.append(k)
+        	emotion_values.append(y)
+       #print(emotion_keys,emotion_values)
+    
+       emotion_table = []
+       #emotion_table.append(["Emotion","Count"])
+    
+       for key,value in zip(emotion_keys,emotion_values):
+         #print(key,value)
+       	 temp = [key,value]
+       	 emotion_table.append(temp)
+       
+       print(emotion_table)
+       
+       return (emotion_table)
